b12960518c86f82a156ce8e02595bac5d4e8daa5
Alexandr Kovalchuk <skateray17@gmail.com>
2018-11-09 00:13:30 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH');
+  next();
+});
+app.options('*', (req, res) => res.end());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${server.address().port}`);
+});
