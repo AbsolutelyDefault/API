07c7c022ddbcedd97ee6ab69edf7c79b412baa72
Nikita <nickselyakh@gmail.com>
2018-12-06 16:51:43 +0300

    TEsts

--- __tests__/authorization.specs.js
+++ __tests__/authorization.specs.js
@@ -6,21 +6,10 @@ import sinon from 'sinon';
 import chaiAsPromised from 'chai-as-promised';
 import JWT from 'jsonwebtoken';
 import { authorization } from '../src/controllers';
-import { server } from '../src/app.js';
-import { User } from '../src/models/user.js'
-import {
-  should
-} from 'should-http';
-import {
-  userInfo
-} from 'os';
 
-import { Column } from '../src/models/column.js'
 chai.use(chaiAsPromised);
 
-const {
-  expect
-} = chai;
+const { expect } = chai;
 
 chai.use(chaiHttp);
 
@@ -29,12 +18,12 @@ describe('Authorization', () => {
     it('it should call next with valid auth token', async () => {
       const spy = sinon.spy();
       const testToken = JWT.sign({
-        test: '123'
+        test: '123',
       }, process.env.JWT_SECRET);
       await authorization({
         headers: {
-          authorization: testToken
-        }
+          authorization: testToken,
+        },
       }, null, spy);
       expect(spy.callCount).to.be.equal(1);
     });
@@ -42,10 +31,9 @@ describe('Authorization', () => {
       const spy = sinon.spy();
       return expect(authorization({
         headers: {
-          authorization: 'asfasf'
-        }
+          authorization: 'asfasf',
+        },
       }, null, spy)).to.be.rejected;
     });
   });
 });
-
--- __tests__/columncrud.specs.js
+++ __tests__/columncrud.specs.js
@@ -2,47 +2,27 @@ import '@babel/polyfill';
 import '../src/utils/dotenv';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import sinon from 'sinon';
 import chaiAsPromised from 'chai-as-promised';
 import JWT from 'jsonwebtoken';
-import {
-  authorization
-} from '../src/controllers';
-import {
-  server
-} from '../src/app.js';
-import {
-  User
-} from '../src/models/user.js'
-import {
-  should
-} from 'should-http';
-import {
-  userInfo
-} from 'os';
+import { server } from '../src/app';
+import { User, Column } from '../src/models';
 
-import {
-  Column
-} from '../src/models/column.js'
 chai.use(chaiAsPromised);
 
-const {
-  expect
-} = chai;
+// const { expect } = chai;
 
 chai.use(chaiHttp);
 
 
 describe('Column', () => {
-
   describe('/GET column', () => {
     const user = new User({
       displayName: 'Zalupa',
       googleId: 'kalsndlk123l;k',
-      nickname: 'piska'
+      nickname: 'piska',
     });
     const testToken = JWT.sign({
-      mongoId: user._id
+      mongoId: user._id,
     }, process.env.JWT_SECRET);
     it('it should GET сolumn', (done) => {
       chai.request(server)
@@ -59,44 +39,23 @@ describe('Column', () => {
     const user = new User({
       displayName: 'Zalupa',
       googleId: 'kalsndlk123l;k',
-      nickname: 'piska'
+      nickname: 'piska',
     });
     const testToken = JWT.sign({
-      mongoId: user._id
+      mongoId: user._id,
     }, process.env.JWT_SECRET);
-    it('it should not POST a column without authorId and tasks', (done) => {
-      const column = {
-        name: 'BOY',
-      }
-      chai.request(server)
-        .post('/api/column/')
-        .set('authorization', testToken)
-        .send(column)
-        .end((err, res) => {
-          res.should.have.status(200);
-          // res.body.should.be.a('object');
-          // res.body.should.have.property('errors');
-          // res.body.errors.should.have.property('pages');
-          // res.body.errors.pages.should.have.property('kind').eql('required');
-          done();
-        });
-    });
     it('it should POST a column ', (done) => {
       const column = {
         name: 'BOY',
         authorId: user._id,
-        tasks: []
-      }
+        tasks: [],
+      };
       chai.request(server)
         .post('/api/column/')
         .set('authorization', testToken)
         .send(column)
         .end((err, res) => {
           res.should.have.status(200);
-          // res.body.should.be.a('object');
-          // res.body.book.should.have.property('name');
-          // res.body.book.should.have.property('authorId');
-          // res.body.book.should.have.property('tasks');
           done();
         });
     });
@@ -106,25 +65,25 @@ describe('Column', () => {
     const user = new User({
       displayName: 'Zalupa',
       googleId: 'kalsndlk123l;k',
-      nickname: 'piska'
+      nickname: 'piska',
     });
     const testToken = JWT.sign({
-      mongoId: user._id
+      mongoId: user._id,
     }, process.env.JWT_SECRET);
     it('it should UPDATE a column given the id', (done) => {
       const column = new Column({
         name: 'BOY',
         authorId: user._id,
-        tasks: []
-      })
-      column.save((err, column) => {
+        tasks: [],
+      });
+      column.save(() => {
         chai.request(server)
           .put('/api/column/')
           .set('authorization', testToken)
           .send({
-            name: 'ARRA'
+            name: 'ARRA',
           })
-          .end((err, res) => {
+          .end((res) => {
             res.should.have.status(200);
             // res.body.should.be.a('object');
             // res.body.should.have.property('message').eql('Book updated!');
@@ -141,22 +100,22 @@ describe('Column', () => {
     const user = new User({
       displayName: 'Zalupa',
       googleId: 'kalsndlk123l;k',
-      nickname: 'piska'
+      nickname: 'piska',
     });
     const testToken = JWT.sign({
-      mongoId: user._id
+      mongoId: user._id,
     }, process.env.JWT_SECRET);
     it('it should DELETE a column given the id', (done) => {
       const column = new Column({
         name: 'ARRA',
         authorId: user._id,
-        tasks: []
-      })
-      column.save((err, column) => {
+        tasks: [],
+      });
+      column.save(() => {
         chai.request(server)
           .delete('/api/column/')
           .set('authorization', testToken)
-          .end((err, res) => {
+          .end((res) => {
             res.should.have.status(200);
             // res.body.should.be.a('object');
             // res.body.should.have.property('message').eql('Book successfully deleted!');
